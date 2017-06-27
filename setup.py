184ab64ed6f83f1dae4063d541481402e76422fc
kiyoto1022 <kiyoto1022@gmail.com>
2017-06-28 00:21:13 +0900

    setup.pyの追加

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='fizz_buzz',
+    version='1.0',
+    description='Enjoy FizzBuzz.',
+    author='Kiyoto Akiyama',
+    author_email='kiyoto@akiyama.co',
+    url='https://github.com/kiyoto1022/fizzbuzz',
+    packages=['fizz_buzz']
+)
