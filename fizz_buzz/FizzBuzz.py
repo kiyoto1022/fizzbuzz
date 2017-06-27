9d664c1c1273eaa0c4978acb6da06fe57eeae088
kiyoto1022 <kiyoto1022@gmail.com>
2017-06-28 00:04:02 +0900

    FizzBuzzクラスを新規追加

---
+++ fizz_buzz/FizzBuzz.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class FizzBuzz:
+
+    _DEFAULT_SEPARATE = "\n"
+
+    def __init__(self, stop):
+        self.__stop = stop
+        self.__separate = self._DEFAULT_SEPARATE
+
+    def __is_fizz(self, num):
+        if num % 3 == 0:
+            return True
+        return False
+
+    def __is_buzz(self, num):
+        if num % 5 == 0:
+            return True
+        return False
+
+    def __judge(self, num):
+        if self.__is_fizz(num) and self.__is_buzz(num):
+            return "FizzBuzz"
+        if self.__is_fizz(num):
+            return "Fizz"
+        if self.__is_buzz(num):
+            return "Buzz"
+        return str(num)
+
+    def set_separate(self, separate):
+        self.__separate = separate
+
+    def say(self):
+        say = ""
+        for i in range(int(self.__stop)):
+            answer = self.__judge(i + 1)
+            say += answer + self.__separate
+        return say
